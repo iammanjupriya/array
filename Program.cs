84031d6d9d4933bc7e03140e7166693f6cdaa410
manju priya <106304226+iammanjupriya@users.noreply.github.com>
2023-07-20 10:39:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int[] ar = new int[100];
+            int i, n, sum = 0;
+
+
+            Console.Write(" total number of elemnts in the array list is :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+           
+            Console.Write("Input elements in the array :\n", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element - {0} : ", i);
+                ar[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i = 0; i < n; i++)
+            {
+                sum += ar[i];
+            }
+            Console.WriteLine("sum of the array is : " + sum);
+
+        }
+
+
+
+      
+        }
+    }
+
+
